1cc9057462fb31dd8cd931bd409d5fdb17875857
orggue <jorgechato1@gmail.com>
2015-07-19 19:35:59 +0200

    footer

---
+++ src/scripts/directives/footer.js
@@ -0,0 +1,11 @@
+angular.module('orggue')
+.directive('orgguefooter',function(Config){
+  return{
+    restrict : 'E',
+    replace : true,
+    templateUrl : Config.baseViews + 'element/footer.html',
+    controller : function($scope,Colors){
+      $scope.color = Colors.getHeaderColor();
+    }
+  };
+});
