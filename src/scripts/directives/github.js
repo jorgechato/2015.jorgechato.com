6d6c13dea45b3778c3434703133b5b73bcabd9fa
orggue <jorgechato1@gmail.com>
2015-07-26 14:48:46 +0200

    control mobile conexions

--- src/scripts/directives/github.js
+++ src/scripts/directives/github.js
@@ -9,6 +9,12 @@ angular.module('orggue')
       $scope.languageColor = function(language){
         return Colors.getColor(language);
       };
+
+      $scope.isMobile = function() {
+        if( navigator.userAgent.match(/Android/i) || navigator.userAgent.match(/webOS/i) || navigator.userAgent.match(/iPhone/i) || navigator.userAgent.match(/iPad/i) || navigator.userAgent.match(/iPod/i) || navigator.userAgent.match(/BlackBerry/i) || navigator.userAgent.match(/Windows Phone/i)){
+          return true;
+        }else return false;
+      };
     }
   };
 });
